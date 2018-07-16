bbfdea1af1831c92cde14ab198e09ebd884a6893
Alice2458 <18845788065@163.com>
2018-07-16 08:59:10 +0800

    m

--- app/src/main/java/com/example/alice/service/LoginService.java
+++ app/src/main/java/com/example/alice/service/LoginService.java
@@ -8,7 +8,7 @@ import java.io.FileOutputStream;
 public class LoginService {
     public static boolean saveUserInfo(Context context,String username, String password){
         try{
-            File f = new File(context.getFilesDir(),"infor.txt");
+            File f = new File(context.getCacheDir(),"infor.txt");
             FileOutputStream fos = new FileOutputStream(f);
             fos.write((username + "##" + password).getBytes());
             fos.close();
